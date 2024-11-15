9e3e3f5ece10118c57bd8f29f3799c71d132dc03
mohdaizat34 <52664189+mohdaizat34@users.noreply.github.com>
2024-11-15 22:41:06 +0800

    finish question 3 js

--- javascript/question-4/question-4.js
+++ javascript/question-4/question-4.js
@@ -1,2 +1,12 @@
 // Write a function that removes all spaces from a given string.
 
+
+let result; 
+
+function removeSpace(input){
+    result = input.replace(/\s+/g, '')
+    
+    return result 
+}
+
+console.log(removeSpace("I love coding")); 
